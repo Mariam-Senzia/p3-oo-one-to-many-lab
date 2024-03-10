709be5ed93937a6b12cf8e5a19cd2d933f95c6cc
Mariam Senzia <mariamsenzia@gmail.com>
2024-03-10 09:18:40 +0300

    One to many relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        if pet_type not in self.PET_TYPES:
+            raise TypeError("Pet_type must be included in PET_TYPES")
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name 
+
+    def pets(self): 
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if pet.pet_type in Pet.PET_TYPES:
+            pet.owner = self
+            Pet.all.append(pet)
+
+    def pet_name(self, pet):
+        return pet.name
+
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key=self.pet_name)
+    
+owner = Owner("John")
+pet = Pet("Fluffy", "dog", owner)
+
